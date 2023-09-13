5b49d3ffe18ea102f5513e3d1a0975cb9f7c293e
Vontell <sankarsh.rajeev@gmail.com>
2023-09-13 14:25:48 -0700

    some files added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -17,9 +17,19 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-  <html lang="en">
-    <body className={inter.className}>{children}</body>
-  </html>
-</ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+
+            <RightSidebar />
+
+          </main>
+          
+          <Bottombar/>
+          </body>
+      </html>
+    </ClerkProvider>
   )
 }
